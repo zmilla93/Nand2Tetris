fc2505a59b826245f7b9d1a7668e384a6fe0dc87
Zach Miller <zmilla93@gmail.com>
2023-06-15 17:12:13 -0500

    fixed command line argument handling

--- compiler/Assembler/src/com/zrmiller/assembler/Main.java
+++ compiler/Assembler/src/com/zrmiller/assembler/Main.java
@@ -6,23 +6,28 @@ import java.io.PrintWriter;
 
 public class Main {
 
-    static String inputFile = "D:\\Programming\\Test Programs\\compile\\Rect.asm";
+    static String inputFile = "";
     static String outputFile = "";
     static PrintWriter output;
 
-    static boolean debugText = true;
-    static int debugMaxLine = 999999;
+    static boolean debugText = false;
 
     public static void main(String[] args) {
 
-        // File handling
+        // Argument handling
         if (args.length > 0 || !inputFile.equals("")) {
+            // Input file
             if (args.length > 0) {
                 inputFile = args[0];
             }
             File inputFile = new File(Main.inputFile);
-            if (args.length > 1) {
+            // Output file
+            if (args.length > 1 && !args[1].equals("-debug")) {
                 outputFile = args[1];
+                if (!outputFile.endsWith(".hack")) {
+                    System.out.println("Output file must be a .hack file! Assembly aborted.");
+                    System.exit(0);
+                }
             } else if (inputFile.isFile()) {
                 outputFile = Main.inputFile.replaceAll("\\.asm", ".hack");
             }
@@ -35,72 +40,82 @@ public class Main {
             } catch (FileNotFoundException e) {
                 System.out.println("Invalid output file name : " + Main.outputFile);
             }
-
-            // Create parser
-            Parser p = new Parser(Main.inputFile);
-            SymbolTable sym = new SymbolTable();
-            System.out.println("Assembler started...");
-
-            // First Pass - Adds all symbols to symbol table
-            System.out.println("First pass, adding symbols to symbol table...");
-            while (p.hasMoreCommands()) {
-                p.advance();
-                if (p.commandType() == CommandType.L) {
-                    sym.addEntry(p.symbol(), p.lineCount());
-                    if (debugText) System.out.println(" --- " + p.symbol());
-                }
+            // Debug text
+            if (args.length > 1) {
+                String target = args[1];
+                if (args.length > 2) target = args[2];
+                if (target.equals("-debug")) debugText = true;
             }
-            // Second Pass
-            p = new Parser(Main.inputFile);
-            System.out.println("Second pass, generating machine code...");
-            if(debugText){
-                System.out.println(getFormattedLine(new String[]{"Line", "Text", "Type", "Dest", "Comp", "Jump"}));
-                System.out.println(getFormattedLine(new String[]{"----", "----", "----", "----", "----", "----"}));
-            }
-            while (p.hasMoreCommands()) {
-                p.advance();
-                if (!p.rawText().equals("")) {
-                    StringBuilder line = new StringBuilder();
-                    if (debugText && p.lineCount() <= debugMaxLine) {
-                        String debugD = p.dest() != null ? p.dest() : "NULL";
-                        String debugC = p.comp() != null ? p.comp() : "NULL";
-                        String debugJ = p.jump() != null ? p.jump() : "NULL";
-                        String[] debugArr = {"(" + p.lineCount() + ")", p.rawText(), p.commandType().toString(), debugD, debugC, debugJ};
-                        System.out.println(getFormattedLine(debugArr));
-                    }
-                    switch (p.commandType()) {
-                        case A:
-                            if (p.symbol().matches("\\d+")) {
-                                line.append(Integer.toBinaryString(Integer.parseInt(p.symbol())));
-                            } else {
-                                if (!sym.contains(p.symbol())) {
-                                    sym.addEntry(p.symbol());
-                                }
-                                int i = sym.getAddress(p.symbol());
-                                line.append(Integer.toBinaryString(i));
-                            }
-                            while (line.length() < 16) {
-                                line.insert(0, "0");
-                            }
-                            break;
-                        case L:
-                            break;
-                        case C:
-                            line = new StringBuilder("111" + Code.comp(p.comp()) + Code.dest(p.dest()) + Code.jump(p.jump()));
-                            break;
-                        default:
-                            break;
-                    }
-                    if (!line.toString().equals("")) {
-                        output.write(line + "\n");
-                    }
-                }
-            }
-            p.close();
-            output.close();
-            System.out.println("Line count : " + p.lineCount());
-            System.out.println("Assembly completed successfully!");
+        } else {
+            System.out.println("No input file specified! Assembly aborted.");
+            System.exit(0);
         }
+
+        // Create parser
+        Parser p = new Parser(Main.inputFile);
+        SymbolTable sym = new SymbolTable();
+        System.out.println("Assembler started...");
+
+        // First Pass - Adds all symbols to symbol table
+        System.out.println("First pass, adding symbols to symbol table...");
+        while (p.hasMoreCommands()) {
+            p.advance();
+            if (p.commandType() == CommandType.L) {
+                sym.addEntry(p.symbol(), p.lineCount());
+                if (debugText) System.out.println(" --- " + p.symbol());
+            }
+        }
+
+        // Second Pass
+        p = new Parser(Main.inputFile);
+        System.out.println("Second pass, generating machine code...");
+        if (debugText) {
+            System.out.println(getFormattedLine(new String[]{"Line", "Text", "Type", "Dest", "Comp", "Jump"}));
+            System.out.println(getFormattedLine(new String[]{"----", "----", "----", "----", "----", "----"}));
+        }
+        while (p.hasMoreCommands()) {
+            p.advance();
+            if (!p.rawText().equals("")) {
+                StringBuilder line = new StringBuilder();
+                if (debugText) {
+                    String debugD = p.dest() != null ? p.dest() : "NULL";
+                    String debugC = p.comp() != null ? p.comp() : "NULL";
+                    String debugJ = p.jump() != null ? p.jump() : "NULL";
+                    String[] debugArr = {"(" + p.lineCount() + ")", p.rawText(), p.commandType().toString(), debugD, debugC, debugJ};
+                    System.out.println(getFormattedLine(debugArr));
+                }
+                switch (p.commandType()) {
+                    case A:
+                        if (p.symbol().matches("\\d+")) {
+                            line.append(Integer.toBinaryString(Integer.parseInt(p.symbol())));
+                        } else {
+                            if (!sym.contains(p.symbol())) {
+                                sym.addEntry(p.symbol());
+                            }
+                            int i = sym.getAddress(p.symbol());
+                            line.append(Integer.toBinaryString(i));
+                        }
+                        while (line.length() < 16) {
+                            line.insert(0, "0");
+                        }
+                        break;
+                    case C:
+                        line = new StringBuilder("111" + Code.comp(p.comp()) + Code.dest(p.dest()) + Code.jump(p.jump()));
+                        break;
+                    case L: // L commands are symbols and do not produce any machine code
+                    default:
+                        break;
+                }
+                if (!line.toString().equals("")) {
+                    output.write(line + "\n");
+                }
+            }
+        }
+        p.close();
+        output.close();
+        System.out.println("Line count : " + p.lineCount());
+        System.out.println("Assembly completed successfully!");
+
     }
 
     public static String getFormattedLine(String[] in) {
