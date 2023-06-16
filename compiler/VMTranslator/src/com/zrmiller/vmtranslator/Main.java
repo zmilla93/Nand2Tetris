d5f6d2ce4780c4d8162f3ea736de8adbedf003cf
Zach Miller <zmilla93@gmail.com>
2023-06-16 16:09:23 -0500

    improved debugging

--- compiler/VMTranslator/src/com/zrmiller/vmtranslator/Main.java
+++ compiler/VMTranslator/src/com/zrmiller/vmtranslator/Main.java
@@ -1,105 +1,115 @@
 package com.zrmiller.vmtranslator;
 
 import java.io.File;
-import java.io.IOException;
-
-import javax.print.attribute.standard.PDLOverrideSupported;
+import java.util.Objects;
 
 public class Main {
 
-	private static String fileInString = "D:\\Programming\\Test Programs\\compile";
-	private static String fileOutString = "";
-	private static int fileCount = 0;
-	private static String[] fileInStringArray;
-	
-	public static void main(String[] args){
-		
-		System.out.println("VM STARTED...");
-		File fileIn = new File(fileInString);
-		if(fileIn.isFile() && fileInString.matches(".+\\.vm\\z")){
-			fileInStringArray = new String[1];
-			fileInStringArray[0] = fileInString;
-			fileOutString = fileInString.replaceAll("\\.vm", ".asm");
-		}else if(fileIn.isDirectory()){
-			//System.out.println(fileIn.getName());
-			fileOutString = fileInString + "\\" + fileIn.getName() + ".asm";
-			for(File f : fileIn.listFiles()){
-				if(f.getName().matches(".+\\.vm\\z")){
-					fileCount++;
-				}
-			}
-			fileInStringArray = new String[fileCount];
-			int i = 0;
-			for(File f : fileIn.listFiles()){
-				if(f.getName().matches(".+\\.vm\\z")){
-					fileInStringArray[i] = f.getAbsolutePath();
-					i++;
-				}
-			}
-		}else{
-			System.out.println("[MAIN] No file or directory found at \"" + fileInString + "\"\nTerminating Virtual Machine");
-			System.exit(0);
-		}
-		CodeWriter code = new CodeWriter(fileOutString, false, false);
-		for(String s : fileInStringArray){
-			System.out.println("\nPARSING NEW FILE... \"" + s + "\"...");
-			Parser p = new Parser(s);
-			code.setFileName(s);
-			//TODO: move to Code
-			while(p.hasMoreCommands()){
-				p.advance();
-				if(!p.getRawText().equals("")){
-					switch(p.commandType()){
-					case C_PUSH:
-					case C_POP:
-						code.writePushPop(p.commandType(), p.arg1(), p.arg2());
-						break;
-					case C_ARITHMETIC:
-						code.writeArithmetic(p.arg1());
-						break;
-					case C_LABEL:
-						code.writeLabel(p.arg1());
-						break;
-					case C_GOTO:
-						code.writeGoto(p.arg1());
-						break;
-					case C_IF:
-						code.writeIf(p.arg1());
-						break;
-					case C_FUNCTION:
-						code.writeFunction(p.arg1(), p.arg2());
-						break;
-					case C_CALL:
-						code.writeCall(p.arg1(), p.arg2());
-						break;
-					case C_RETURN:
-						code.writeReturn();
-						break;
-					default:
-						System.out.println("Unregistered Command : " + p.commandType());
-					}
-				String[] debug = {p.getRawText(), p.commandType().toString()};
-				System.out.println(debugText(debug));
-				//code.breakPoint();
-				}
-			}
-		}
-		code.close();
+    private static String fileInString = "D:\\Programming\\Test\\compile";
+    private static String fileOutString = "";
+    private static int fileCount = 0;
+    private static String[] fileInStringArray;
+    private static boolean debug = false;
 
-	}
-	
-	private static String debugText(String[] text){
-		String ret = " ----- ";
-		int columnWidth = 30;
-		for(String s : text){
-			ret += s;
-			int i = s.length();
-			while(i<columnWidth){
-				ret += " ";
-				i++;
-			}
-		}
-		return ret;
-	}
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        System.out.println("Virtual machine started...");
+        if (args.length > 0) fileInString = args[0];
+        for (String s : args) {
+            if (s.equals("-debug")) {
+                debug = true;
+                break;
+            }
+        }
+        File fileIn = new File(fileInString);
+        if (fileIn.isFile() && fileInString.matches(".+\\.vm\\z")) {
+            // Single file
+            fileInStringArray = new String[1];
+            fileInStringArray[0] = fileInString;
+            fileOutString = fileInString.replaceAll("\\.vm", ".asm");
+        } else if (fileIn.isDirectory()) {
+            // Directory
+            fileOutString = fileInString + "\\" + fileIn.getName() + ".asm";
+            for (File f : Objects.requireNonNull(fileIn.listFiles())) {
+                if (f.getName().matches(".+\\.vm\\z")) {
+                    fileCount++;
+                }
+            }
+            fileInStringArray = new String[fileCount];
+            int i = 0;
+            for (File f : Objects.requireNonNull(fileIn.listFiles())) {
+                if (f.getName().matches(".+\\.vm\\z")) {
+                    fileInStringArray[i] = f.getAbsolutePath();
+                    i++;
+                }
+            }
+        } else {
+            // Invalid input file/directory
+            System.out.println("No file or directory found at \"" + fileInString + "\"\nTerminating Virtual Machine");
+            System.exit(0);
+        }
+        CodeWriter code = new CodeWriter(fileOutString, false, false);
+        for (String s : fileInStringArray) {
+            System.out.println("\tParsing file \"" + s + "\"...");
+            Parser p = new Parser(s);
+            code.setFileName(s);
+            while (p.hasMoreCommands()) {
+                p.advance();
+                if (!p.getRawText().equals("")) {
+                    switch (p.commandType()) {
+                        case C_PUSH:
+                        case C_POP:
+                            code.writePushPop(p.commandType(), p.arg1(), p.arg2());
+                            break;
+                        case C_ARITHMETIC:
+                            code.writeArithmetic(p.arg1());
+                            break;
+                        case C_LABEL:
+                            code.writeLabel(p.arg1());
+                            break;
+                        case C_GOTO:
+                            code.writeGoto(p.arg1());
+                            break;
+                        case C_IF:
+                            code.writeIf(p.arg1());
+                            break;
+                        case C_FUNCTION:
+                            code.writeFunction(p.arg1(), p.arg2());
+                            break;
+                        case C_CALL:
+                            code.writeCall(p.arg1(), p.arg2());
+                            break;
+                        case C_RETURN:
+                            code.writeReturn();
+                            break;
+                        default:
+                            System.out.println("Unregistered Command : " + p.commandType());
+                    }
+                    String[] debugText = {p.getRawText(), p.commandType().toString(), p.arg1(), p.arg2String()};
+                    if (debug) System.out.println(debugText(debugText));
+                }
+            }
+            if (debug) System.out.println("");
+        }
+        code.close();
+        System.out.println("Output written to \"" + fileOutString + "\".");
+        time = System.currentTimeMillis() - time;
+        System.out.println("Virtual machine finished in " + time + " milliseconds.");
+    }
+
+    private static String debugText(String[] text) {
+        StringBuilder ret = new StringBuilder("\t\t - ");
+        int columnWidth = 30;
+        for (String s : text) {
+            if(s == null) s = "NULL";
+            ret.append(s);
+            int length = s.length();
+            while (length < columnWidth) {
+                ret.append(" ");
+                length++;
+            }
+        }
+        return ret.toString();
+    }
 
 }
--- compiler/VMTranslator/src/com/zrmiller/vmtranslator/Parser.java
+++ compiler/VMTranslator/src/com/zrmiller/vmtranslator/Parser.java
@@ -7,98 +7,104 @@ import java.io.IOException;
 
 public class Parser {
 
-	private FileReader fr;
-	private BufferedReader br;
-	private String curLine = "";
-	private String[] curLineArray;
-	private CommandType cmdType;
-	
-	public Parser(String fileName){
-		try {
-			fr = new FileReader(fileName);
-		} catch (FileNotFoundException e) {
-			System.out.println("[PARSER] File not found \"" + fileName + "\"\nTerminating Virtual Machine");
-			System.exit(0);
-		}
-		br = new BufferedReader(fr);
-	}
-	
-	public boolean hasMoreCommands(){
-		try {
-			return br.ready();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-	
-	public void advance(){
-		try {
-			//Removes end of line comments and standardized all white spaces
-			curLine = br.readLine().replaceAll("\\/\\/.*", "").replaceAll("(\\A\\s+|\\s+\\z)", "").replaceAll("\\s+", " ");
-			curLineArray = curLine.split("[ ]+");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public String getRawText(){
-		return curLine;
-	}
-	
-	public CommandType commandType(){
-		switch(curLineArray[0]){
-		case "add":
-		case "sub":
-		case "neg":
-		case "eq":
-		case "gt":
-		case "lt":
-		case "and":
-		case "or":
-		case "not":
-			cmdType = CommandType.C_ARITHMETIC;
-			break;
-		case "push":
-			cmdType = CommandType.C_PUSH;
-			break;
-		case "pop":
-			cmdType = CommandType.C_POP;
-			break;
-		case "label":
-			cmdType = CommandType.C_LABEL;
-			break;
-		case "goto":
-			cmdType = CommandType.C_GOTO;
-			break;
-		case "if-goto":
-			cmdType = CommandType.C_IF;
-			break;
-		case "function":
-			cmdType = CommandType.C_FUNCTION;
-			break;
-		case "return":
-			cmdType = CommandType.C_RETURN;
-			break;
-		case "call":
-			cmdType = CommandType.C_CALL;
-			break;
-		default:
-			cmdType = CommandType.ERROR;
-			break;
-		}
-		return cmdType;
-	}
-	
-	public String arg1(){
-		if(cmdType == CommandType.C_ARITHMETIC){
-			return curLineArray[0];
-		}
-		return curLineArray[1];
-	}
-	
-	public int arg2(){
-		return Integer.parseInt(curLineArray[2]);
-	}
-	
+    private FileReader fr;
+    private BufferedReader br;
+    private String curLine = "";
+    private String[] curLineArray;
+    private CommandType cmdType;
+
+    public Parser(String fileName) {
+        try {
+            fr = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("[PARSER] File not found \"" + fileName + "\"\nTerminating Virtual Machine");
+            System.exit(0);
+        }
+        br = new BufferedReader(fr);
+    }
+
+    public boolean hasMoreCommands() {
+        try {
+            return br.ready();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void advance() {
+        try {
+            //Removes end of line comments and standardized all white spaces
+            curLine = br.readLine().replaceAll("\\/\\/.*", "").replaceAll("(\\A\\s+|\\s+\\z)", "").replaceAll("\\s+", " ");
+            curLineArray = curLine.split("[ ]+");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getRawText() {
+        return curLine;
+    }
+
+    public CommandType commandType() {
+        switch (curLineArray[0]) {
+            case "add":
+            case "sub":
+            case "neg":
+            case "eq":
+            case "gt":
+            case "lt":
+            case "and":
+            case "or":
+            case "not":
+                cmdType = CommandType.C_ARITHMETIC;
+                break;
+            case "push":
+                cmdType = CommandType.C_PUSH;
+                break;
+            case "pop":
+                cmdType = CommandType.C_POP;
+                break;
+            case "label":
+                cmdType = CommandType.C_LABEL;
+                break;
+            case "goto":
+                cmdType = CommandType.C_GOTO;
+                break;
+            case "if-goto":
+                cmdType = CommandType.C_IF;
+                break;
+            case "function":
+                cmdType = CommandType.C_FUNCTION;
+                break;
+            case "return":
+                cmdType = CommandType.C_RETURN;
+                break;
+            case "call":
+                cmdType = CommandType.C_CALL;
+                break;
+            default:
+                cmdType = CommandType.ERROR;
+                break;
+        }
+        return cmdType;
+    }
+
+    public String arg1() {
+        if (cmdType == CommandType.C_ARITHMETIC) {
+            return curLineArray[0];
+        }
+        if (curLineArray.length < 2) return null;
+        return curLineArray[1];
+    }
+
+    public int arg2() {
+        return Integer.parseInt(curLineArray[2]);
+    }
+
+    public String arg2String() {
+        if (curLineArray.length < 3) return null;
+        return curLineArray[2];
+    }
+
 }
